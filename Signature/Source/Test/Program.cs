0f8fbf933a3fd98b740af6c3c98390b20568c6ef
qiu.yanjun <qiu.yanjun@jinyinmao.com.cn>
2017-09-29 20:09:13 +0800

    add new pro

---
+++ Signature/Source/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using JYM_SignatureContract;
+
+namespace Jinyinmao.Signature.Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            SignContractService service = new SignContractService();
+
+            service.Start();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Signature/Source/jinyinmao.Signature.ServiceTests/FDDServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jinyinmao.Signature.Service.Tests
+{
+    [TestClass]
+    public class FDDServiceTests
+    {
+        [TestMethod]
+        public void ClearContractFileTest()
+        {
+        }
+
+        [TestMethod]
+        public void FDDServiceTest()
+        {
+            try
+            {
+                FDDService service = new FDDService();
+
+                Assert.IsNotNull(service);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("实例化FDDService出错:" + ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void GenerationSigleOrderContractAsyncTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void GenerationSigleProductContractAsyncTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void GetProductOrderInfoAsyncTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void GetUserIdByOrderIdTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void SendTirifalAsyncTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void UploadContractTemplateAsyncTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
